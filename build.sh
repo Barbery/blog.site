9ceecca260f7a88be7b1c70b9cb5ef4b5aeadc86
barbery <barbery@qingtengonline.com>
2016-08-25 16:16:02 +0800

    release article

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@ git add *
 git commit -m "release article"
 git push
 hugo -t hugo-pacman-theme
-rm -rf /tmp/public
-mv public /tmp/public
+sudo rm -rf /tmp/public
+sudo mv public /tmp/public
 git checkout gh-pages
 shopt -s extglob
 rm -rf !(CNAME)
-mv /tmp/public/* ./
+sudo mv /tmp/public/* ./
 
 git add *
 git commit -m "publish"
